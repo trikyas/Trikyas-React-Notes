1fa1e20a1c652aff2541b0e92f7c906618e61751
trikyas <mooneychad@yahoo.com>
2017-04-25 00:37:34 +1000

    making a Note app in react

---
+++ src/Board.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import './App.css'
+import Note from './Note'
+
+var Board = React.createClass({
+propTypes: {
+  count: function(props, propName) {
+    if(typeof props[propName] !== "number") {
+      return new Error("the count must be a number")
+    }
+    if (props[propName] > 100) {
+      return new Error('Seriously ' + props[propName]
+    + ' notes is ridiculous')
+    }
+  }
+},
+getInitialState() {
+  return {
+    notes: [ ]
+  }
+},
+nextId() {
+  this.uniqueId = this.uniqueId || 0
+  return this.uniqueId++
+},
+add(text) {
+  var notes = [
+    ...this.state.notes,
+    {
+      id: this.nextId(),
+      note: text
+    }
+  ]
+  this.setState({notes})
+},
+update(newText, id) {
+  var notes = this.state.notes.map(
+    note => (note.id !== id) ?
+    note :
+    {
+      ...note,
+      note: newText
+    }
+  )
+  this.setState({notes})
+},
+remove(id) {
+  var notes =  this.state.notes.filter(note => note.id !== id)
+  this.setState({notes})
+},
+eachNote(note) {
+    return ( <Note key={note.id} id={note.id} onChange={this.update} onRemove={this.remove}>{note.note}</Note>)
+},
+  render() {
+    return (<div className="board">
+      {this.state.notes.map(this.eachNote)}
+      <button onClick={() => this.add('New Note')}>+</button>
+    </div>)
+  }
+})
+
+export default Board
